b744b6bde61e9240c7cdcb6607e791e56e06ba9e
YongYi01 <155723008+YongYi01@users.noreply.github.com>
2024-01-19 19:08:38 +0800

    Update install_packages.py

--- install_packages.py
+++ install_packages.py
@@ -11,3 +11,9 @@ Original file is located at
 !pip install apache-airflow
 # Initialize the Airflow database
 !airflow initdb
+
+# Import necessary libraries
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import pandas as pd
