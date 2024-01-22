cff2faf4aff259cba25baf9d42b7b842a53b9919
YongYi01 <155723008+YongYi01@users.noreply.github.com>
2024-01-22 17:00:41 +0800

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -1,12 +1,11 @@
 # Step 1: Data Exploration
-def data_exploration():
-    print("Step 1: Data Exploration")
-    #Target variable: weather_code (since it indicates different weather condition
-    data = '/home/ubuntu/project/um_wqd7008/hourly_weather.csv'
-    df = pd.read_csv(data)
-    # Display basic information about the dataset
-    print(df.info())
-    # Display the first few rows of the dataset
-    print(df.head())
 
-data_exploration()
+print("Step 1: Data Exploration")
+#Target variable: weather_code (since it indicates different weather condition
+data = '/home/ubuntu/project/um_wqd7008/hourly_weather.csv'
+df = pd.read_csv(data)
+# Display basic information about the dataset
+print(df.info())
+# Display the first few rows of the dataset
+print(df.head())
+
